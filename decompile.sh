746a36619d3f51ecbb14d88e9bade17b86c27667
tre0521 <ssbigtree@gmail.com>
2024-11-28 14:42:49 +0800

    docker

--- decompile.sh
+++ decompile.sh
@@ -6,6 +6,7 @@ cd /app
 # 检查 pak01_dir.vpk 文件是否存在
 if [ -f "./temp/pak01_dir.vpk" ]; then
     # 赋予 Decompiler 可执行权限
+
     chmod +x ./Decompiler
     # 执行 Decompiler 解包操作
     ./Decompiler -i "./temp/pak01_dir.vpk" -o "./static" -e "vtex_c" -d -f "panorama/images/econ"
